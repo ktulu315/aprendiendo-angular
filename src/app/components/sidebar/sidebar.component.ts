9181e5e8c4802fd1f30c28a8fe9cf17017b64ffc
David Eduardo Gualteros <ktulu315@gmail.com>
2025-11-24 23:53:46 -0500

    Correccion de errores en sidebar

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
+  standalone: true,
   selector: 'app-sidebar',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit{
+  
+  public searchString: string;
+
+  constructor(){
+    this.searchString = '';
+  }
+
+  ngOnInit(){
+    
+  }
+
+  goSearch(){
+    alert(this.searchString)
+  }
 
 }
